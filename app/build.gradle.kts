e8210ac2b69a5826e0f7d8957a1c7f40c1a07ab0
MhmdIbrahim1 <107378571+MhmdIbrahim1@users.noreply.github.com>
2023-12-21 02:09:39 +0200

    LOL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         minSdk = 21
         targetSdk = 33 /* Android 14 is Fu*ked
         ^ https://developer.android.com/about/versions/14/behavior-changes-14#safer-dynamic-code-loading*/
-        versionCode = 66
-        versionName = "4.3.3"
+        versionCode = 67
+        versionName = "4.3.4"
 
         resValue("string", "app_version", "${defaultConfig.versionName}${versionNameSuffix ?: ""}")
         resValue("string", "commit_hash", "git rev-parse --short HEAD".execute() ?: "")
