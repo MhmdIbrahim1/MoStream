e9f82cffbe9ce9a8e6d8e8700d1ce9c5a536881d
MhmdIbrahim1 <107378571+MhmdIbrahim1@users.noreply.github.com>
2023-12-27 01:07:47 +0200

    Lol B2a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ android {
         minSdk = 21
         targetSdk = 33 /* Android 14 is Fu*ked
         ^ https://developer.android.com/about/versions/14/behavior-changes-14#safer-dynamic-code-loading*/
-        versionCode = 68
+        versionCode = 69
         versionName = "4.4.1"
 
         resValue("string", "app_version", "${defaultConfig.versionName}${versionNameSuffix ?: ""}")
