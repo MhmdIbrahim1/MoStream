4c9caa7d455d2c67cc8a0baf1f7a326ed0965486
MhmdIbrahim1 <107378571+MhmdIbrahim1@users.noreply.github.com>
2024-01-02 01:52:38 +0200

    Dumb

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         targetSdk = 33 /* Android 14 is Fu*ked
         ^ https://developer.android.com/about/versions/14/behavior-changes-14#safer-dynamic-code-loading*/
         versionCode = 69
-        versionName = "4.4.2"
+        versionName = "4.4.3"
 
         resValue("string", "app_version", "${defaultConfig.versionName}${versionNameSuffix ?: ""}")
         resValue("string", "commit_hash", "git rev-parse --short HEAD".execute() ?: "")
