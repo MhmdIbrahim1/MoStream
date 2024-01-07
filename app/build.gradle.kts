46ac58aed934fec582f6be75926877a766b054ab
MhmdIbrahim1 <107378571+MhmdIbrahim1@users.noreply.github.com>
2024-01-07 22:20:04 +0200

    Add favorite icon to library bottom dialog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         minSdk = 21
         targetSdk = 33 /* Android 14 is Fu*ked
         ^ https://developer.android.com/about/versions/14/behavior-changes-14#safer-dynamic-code-loading*/
-        versionCode = 69
-        versionName = "4.4.4"
+        versionCode = 70
+        versionName = "4.4.5"
 
         resValue("string", "app_version", "${defaultConfig.versionName}${versionNameSuffix ?: ""}")
         resValue("string", "commit_hash", "git rev-parse --short HEAD".execute() ?: "")
